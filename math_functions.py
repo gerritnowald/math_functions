db4fe4fa4e7c1be1767cb263b6ef2fdcd4cb41c3
Gerrit Nowald <gerrit.nowald@continental-corporation.com>
2022-01-13 10:27:44 +0100

    Create math_functions.py

---
+++ math_functions.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 13 10:21:30 2022
+
+@author: Gerrit Nowald
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# -----------------------------------------------------------------------------
+# trigonometry
+
+def cos_sin(q):                       # q = np.array([x,y])
+    return q/np.sqrt(np.sum(q**2))    # [cos, sin]
+
+# -----------------------------------------------------------------------------
+# plot
+
+def plot_circ( R=1, C=(0,0), color='k', points=50 ):
+    angle = np.linspace(0, 2*np.pi, points)
+    x = C[0] + R*np.cos(angle)
+    y = C[1] + R*np.sin(angle)
+    plt.plot(x,y, color=color )
+    plt.axis('equal')
+
+# -----------------------------------------------------------------------------
+# ODS
+
+def state_space(M,D,C):
+    Minv = np.linalg.inv(M)
+    A = np.vstack((      # state space matrix
+        np.hstack(( np.zeros(np.shape(M)), np.eye(np.shape(M)[1]) )),
+        np.hstack(( - Minv @ C, - Minv @ D )) ))
+    return A, Minv
