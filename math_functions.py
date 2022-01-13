a849212fb12f5038f22018ed1121506800dddf15
Gerrit Nowald <gerrit.nowald@gmail.com>
2022-01-13 13:49:23 +0100

    added polynomial fitting

--- math_functions.py
+++ math_functions.py
@@ -15,21 +15,50 @@ def cos_sin(q):                       # q = np.array([x,y])
     return q/np.sqrt(np.sum(q**2))    # [cos, sin]
 
 # -----------------------------------------------------------------------------
-# plot
+# curve fitting
 
-def plot_circ( R=1, C=(0,0), color='k', points=50 ):
-    angle = np.linspace(0, 2*np.pi, points)
-    x = C[0] + R*np.cos(angle)
-    y = C[1] + R*np.sin(angle)
-    plt.plot(x,y, color=color )
-    plt.axis('equal')
+def polyfit(x,y,N):
+    # polynomial fitting 
+    # x: x-data vector
+    # y: y-data vector, corresponding to x
+    # N: order of fitting polynomial 
+    
+    # coefficient matrix (Vandermonde matrix)
+    A = np.ones((len(x),N+1))
+    for i in reversed(range(0, N)):
+        A[:,i] = x*A[:,i+1]
+    
+    # pseudo-inverse
+    B = np.transpose(A)
+    p = np.linalg.inv( B @ A ) @ ( B @ y )
+        
+    # fitted values
+    yf = np.zeros(np.shape(x))
+    for i in range(N+1):
+        yf += p[N-i]*x**i
+    
+    # p:  polynomial coeffcients in decreasing order
+    # yf: fitted values computed at x
+    return p, yf
 
 # -----------------------------------------------------------------------------
 # ODS
 
 def state_space(M,D,C):
+    # M,D,C: mass, damping and stiffness matrix
     Minv = np.linalg.inv(M)
-    A = np.vstack((      # state space matrix
+    A = np.vstack((
         np.hstack(( np.zeros(np.shape(M)), np.eye(np.shape(M)[1]) )),
         np.hstack(( - Minv @ C, - Minv @ D )) ))
-    return A, Minv
+    return A, Minv  # state space matrix, inverse mass matrix (for force vector)
+
+# -----------------------------------------------------------------------------
+# plot
+
+def plot_circ( R=1, C=(0,0), color='k', points=50 ):
+    # plots a circle with radius R and center C
+    angle = np.linspace(0, 2*np.pi, points)
+    x = C[0] + R*np.cos(angle)
+    y = C[1] + R*np.sin(angle)
+    plt.plot(x,y, color=color )
+    plt.axis('equal')
---
+++ test_polyfit.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 13 10:57:37 2022
+
+@author: Gerrit Nowald
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import math_functions as mf
+    
+
+N = 2   # polynomial order
+
+# test data 
+x = np.linspace(-2,2,100)
+y = np.cos(x)
+y += 0.2*(np.random.rand(np.shape(x)[0])-0.5)   # adding noise
+
+
+# calculation
+p, yf = mf.polyfit(x,y,N)
+
+
+# plot
+plt.close('all')
+
+plt.figure()
+plt.plot(x,y,'.')   # data
+plt.plot(x,yf,'k')  # fitted polynomial
