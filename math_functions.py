1ea610b0e90df2a9a55e3b5ccc4d23357c6cdf0c
Gerrit Nowald <gerrit.nowald@gmail.com>
2022-01-15 14:21:24 +0100

    moved to module

--- math_functions.py
+++ math_functions.py
@@ -15,6 +15,41 @@ def cos_sin(q):                       # q = np.array([x,y])
     return q/np.sqrt(np.sum(q**2))    # [cos, sin]
 
 # -----------------------------------------------------------------------------
+# linear 1D interpolation
+
+def calc_slope(x,y):
+    """
+    slope of line segments for 1D linear inter- and extrapolation
+    - x:  x-data vector [m], monotonically increasing
+    - y:  y-data matrix [m x n], corresponding to x"""
+    if y.ndim ==1:
+        m  = np.diff(y)/np.diff(x)
+    elif y.ndim ==2:
+        m  = np.diff(y,axis=0)/np.diff(x)[:, np.newaxis]
+    return m
+
+def interplinfast(x,y,xi,m=None):
+    """
+    fast 1D linear inter- and extrapolation
+    x:  x-data vector [m], monotonically increasing
+    y:  y-data matrix [m x n], corresponding to x (interpolation for each column)
+    xi: x-value vector [p] to be interpolated, in any order
+    m:  slopes of line segments, can be computed in advance (optional)
+    Loren Shure (The MathWorks) http://blogs.mathworks.com/loren/2008/08/25/piecewise-linear-interpolation/
+    Jose M. Mier, https://de.mathworks.com/matlabcentral/fileexchange/43325-quicker-1d-linear-interpolation-interp1qr """
+    if m.all()==None:
+        m = calc_slope(x,y)             # slope of line segments
+    ind = np.sum( xi > x[:, np.newaxis], axis=0) - 1    # find interval
+    ind = np.maximum(ind, 0)            # avoid index smaller 0
+    ind = np.minimum(ind, len(x)-2)     # avoid index larger than len(x)-2
+    # inter- & extrapolation
+    if y.ndim ==1:
+        yi = m[ind]*(xi-x[ind]) + y[ind]
+    elif y.ndim ==2:
+        yi = m[ind,:]*(xi[:, np.newaxis]-x[ind, np.newaxis]) + y[ind,:]
+    return yi   # interpolated values matrix [p x n], corresponding to xi
+
+# -----------------------------------------------------------------------------
 # curve fitting
 
 def Vandermonde(x,N):
@@ -25,10 +60,11 @@ def Vandermonde(x,N):
     return V
 
 def polyfit(x,y,N):
-    # polynomial fitting using least squares 
-    # x: x-data vector, each value unique
-    # y: y-data vector, corresponding to x
-    # N: order of fitting polynomial 
+    """
+    polynomial fitting using least squares 
+    x: x-data vector, each value unique
+    y: y-data vector, corresponding to x
+    N: order of fitting polynomial """
     V = Vandermonde(x,N)    # coefficient matrix
     B = np.transpose(V)
     p = np.linalg.inv( B @ V ) @ ( B @ y )
--- test_interplinfast.py
+++ test_interplinfast.py
@@ -8,45 +8,7 @@ Created on Sat Jan 15 10:20:05 2022
 import numpy as np
 import matplotlib.pyplot as plt
 
-# -----------------------------------------------------------------------------
-
-def calc_slope(x,y):
-    """
-    slope of line segments for 1D linear inter- and extrapolation
-    - x:  x-data vector [m], monotonically increasing
-    - y:  y-data matrix [m x n], corresponding to x
-          interpolation is performed for each column
-    """
-    if y.ndim ==1:
-        m  = np.diff(y)/np.diff(x)
-    elif y.ndim ==2:
-        m  = np.diff(y,axis=0)/np.diff(x)[:, np.newaxis]
-    return m
-
-def interplinfast(x,y,xi,m=None):
-    """
-    fast 1D linear inter- and extrapolation
-    - x:  x-data vector [m], monotonically increasing
-    - y:  y-data matrix [m x n], corresponding to x
-          interpolation is performed for each column
-    - xi: x-value vector [p] to be interpolated, , in any order
-    - m:  slopes of line segments, can be computed in advance (optional)
-    uses the approach given by Loren Shure (The MathWorks) in http://blogs.mathworks.com/loren/2008/08/25/piecewise-linear-interpolation/
-    Acknowledgement: Jose M. Mier, https://de.mathworks.com/matlabcentral/fileexchange/43325-quicker-1d-linear-interpolation-interp1qr
-    """
-    if m.all()==None:
-        m = calc_slope(x,y)     # slope of line segments
-    
-    ind = np.sum( xi > x[:, np.newaxis], axis=0) - 1    # find interval
-    ind = np.maximum(ind, 0)            # avoid index smaller 0
-    ind = np.minimum(ind, len(x)-2)     # avoid index larger than len(x)-2
-    
-    # inter- & extrapolation
-    if y.ndim ==1:
-        yi = m[ind]*(xi-x[ind]) + y[ind]
-    elif y.ndim ==2:
-        yi = m[ind,:]*(xi[:, np.newaxis]-x[ind, np.newaxis]) + y[ind,:]
-    return yi   # interpolated values matrix [p x n], corresponding to xi
+import math_functions as mf
 
 #--------------------------------------------------------------------------
 # Variables
@@ -64,10 +26,10 @@ xi = np.linspace(-50,900,Nxi)
 #--------------------------------------------------------------------------
 # Interpolation
 
-# yi = interplinfast(x,y,xi)
+# yi = mf.interplinfast(x,y,xi)
 
-m = calc_slope(x,y)
-yi = interplinfast(x,y,xi,m)
+m  = mf.calc_slope(x,y)
+yi = mf.interplinfast(x,y,xi,m)
 
 
 
