8aa6783a755bbe61cf343c15b3318024ea3d29a3
Gerrit Nowald <gerrit.nowald@gmail.com>
2022-04-22 05:47:57 +0200

    if __name__ == "__main__":

--- test_polyfit.py
+++ test_polyfit.py
@@ -17,33 +17,40 @@ N_data = 30     # number of data points
     
 N = 2           # polynomial order, N<<N_data, N<=N_data-1
 
-# -----------------------------------------------------------------------------
-# test data 
 
-data_x = np.linspace(-2,2,N_data)
-data_y = np.cos(data_x)
-data_y += 0.2*(np.random.rand(np.shape(data_x)[0])-0.5)   # adding noise
+def main():    
+    
+    # -----------------------------------------------------------------------------
+    # test data 
+    
+    data_x = np.linspace(-2,2,N_data)
+    data_y = np.cos(data_x)
+    data_y += 0.2*(np.random.rand(np.shape(data_x)[0])-0.5)   # adding noise
+    
+    # -----------------------------------------------------------------------------
+    # calculation
+    
+    p = mf.polyfit(data_x, data_y, N)   # polynomial coeffcients in increasing order
+    
+    pnp = np.polyfit(data_x, data_y, N)
+    print(f'max. difference coefficients {max(abs(p-pnp[::-1]))}')
+    
+    # -----------------------------------------------------------------------------
+    # plot
+    
+    plt.close('all')
+    
+    plot_x = np.linspace(-2,2,100)
+    plot_y = mf.polyeval(plot_x, p)
+    
+    plot_ynp = np.polyval(pnp, plot_x)
+    print(f'max. difference evaluated polynom {max(abs(plot_y-plot_ynp))}')
+    
+    plt.figure()
+    plt.plot(data_x, data_y, '.', label='data')
+    plt.plot(plot_x, plot_y, 'k', label='fitted polynomial')
+    plt.show()
 
-# -----------------------------------------------------------------------------
-# calculation
 
-p = mf.polyfit(data_x, data_y, N)   # polynomial coeffcients in increasing order
-
-pnp = np.polyfit(data_x, data_y, N)
-print(f'max. difference coefficients {max(abs(p-pnp[::-1]))}')
-
-# -----------------------------------------------------------------------------
-# plot
-
-plt.close('all')
-
-plot_x = np.linspace(-2,2,100)
-plot_y = mf.polyeval(plot_x, p)
-
-plot_ynp = np.polyval(pnp, plot_x)
-print(f'max. difference evaluated polynom {max(abs(plot_y-plot_ynp))}')
-
-plt.figure()
-plt.plot(data_x, data_y, '.', label='data')
-plt.plot(plot_x, plot_y, 'k', label='fitted polynomial')
-plt.show()
+if __name__ == "__main__":
+    main()
