6b000d9c7f3493a9bc0853e532cc4c198d63c8d8
Gerrit Nowald <gerrit.nowald@3ds.com>
2023-05-16 17:07:56 +0200

    reduced nesting

--- PointInPolygonTest.jl
+++ PointInPolygonTest.jl
@@ -26,19 +26,22 @@ function isPointInside(vert,point)
 	# A point is in a polygon, if a line from the point to infinity crosses the polygon an odd number of times.
 	# Here, the line goes parallel to the x-axis in positive x-direction.
 	# adapted from https://www.algorithms-and-technologies.com/point_in_polygon/python
-	odd = false    
-	for j in 1:size(vert)[1]-1          # for each edge check if the line crosses
-		i = j + 1                       # next vertex
-		if vert[j,2] != vert[i,2]       # edge not parallel to x-axis (singularity)
-			# point between y-coordinates of edge
-			if (vert[i,2] > point[2]) != (vert[j,2] > point[2])
-				# x-coordinate of intersection
-				Qx = (vert[j,1]-vert[i,1])*(point[2]-vert[i,2])/(vert[j,2]-vert[i,2]) + vert[i,1]
-				if point[1] < Qx        # point left of edge
-				    odd = ! odd         # line crosses edge
-				end
-			end
-		end
+	odd = false
+	for j in 1:size(vert)[1]-1      # for each edge check if the line crosses
+		i = j + 1                   # next vertex
+        # edge parallel to x-axis (singularity)
+		if vert[j,2] == vert[i,2]
+            continue
+        end
+        # point not between y-coordinates of edge
+        if (vert[i,2] > point[2]) == (vert[j,2] > point[2])
+            continue
+        end
+        # x-coordinate of intersection
+        Qx = (vert[j,1]-vert[i,1])*(point[2]-vert[i,2])/(vert[j,2]-vert[i,2]) + vert[i,1]
+        if point[1] < Qx    # point left of edge
+            odd = ! odd     # line crosses edge
+        end
 	end
 	return odd  # point is in polygon (not on the edge) if odd=true
 end
